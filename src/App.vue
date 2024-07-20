c7481471c5d581f0ff17e9e73b016b0b320706ca
krunal882 <krunalvekariya254@gmail.com>
2024-07-20 20:13:02 +0530

    checked for route

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
         <!-- The main content of the page is displayed here -->
         <router-view></router-view>
     <!-- For special pages, only the main content is displayed without the main navigation and footer -->
-    <router-view v-if="isSpecialPage" />
   </div>
 </template>
 
@@ -13,17 +12,7 @@
 export default {
   name: "App",
 
-  computed: {
-    isSpecialPage() {
-      const { path } = this.$route;
-      return (
-        path === "/authentication" ||
-        path === "/forgotPassword" ||
-        path.startsWith("/resetPassword") ||
-        path === "/404"
-      );
-    },
-  },
+
 };
 </script>
 
--- src/routes/exerciseRoutes.ts
+++ src/routes/exerciseRoutes.ts
@@ -3,22 +3,12 @@ import Cookies from "js-cookie";
 import { createRouter, createWebHistory } from "vue-router";
 import MuscleSelector from "@/components/BodyModel/MuscleSelector.vue";
 
-// check if user is logged-in / registered or not
-const ifAuthenticated = (to, from, next) => {
-  if (Cookies.get("token")) {
-    next();
-  } else {
-    next("/authentication");
-  }
-};
-
 //routes
 const routes = [
  
 
       {
         path: "/anatomy",
-        beforeEnter: ifAuthenticated,
         component: MuscleSelector,
         name: "muscleModel",
       },
